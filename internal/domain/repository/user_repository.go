94affa63ccec46837dae7e594812cb784887afbb
chaki8923 <aowp.contact@gmail.com>
2025-06-01 11:31:08 +0900

    feat: usagiテスト

--- internal/domain/repository/user_repository.go
+++ internal/domain/repository/user_repository.go
@@ -6,7 +6,7 @@ import (
 	"meguru-backend/internal/domain/entity"
 	"github.com/google/uuid"
 )
-
+//このコメントアウトはpush前に絶対消すこと
 type UserRepository interface {
 	Create(ctx context.Context, user *entity.User) error
 	GetByEmail(ctx context.Context, email string) (*entity.User, error)
