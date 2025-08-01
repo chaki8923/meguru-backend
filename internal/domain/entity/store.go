479c75ae60e886fe8664d1d29121d07d8821851c
茶木 涼 <>
2025-08-01 13:54:32 +0900

    fix: 店舗登録修正

--- internal/domain/entity/store.go
+++ internal/domain/entity/store.go
@@ -7,11 +7,15 @@ import (
 )
 
 type Store struct {
-	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name"`
-	Prefecture string    `json:"prefecture"`
-	City       string    `json:"city"`
-	Street     string    `json:"street"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Email       string    `json:"email"`
+	Password    string    `json:"-"`
+	PhoneNumber string    `json:"phone_number"`
+	Zipcode     string    `json:"zipcode"`
+	Prefecture  string    `json:"prefecture"`
+	City        string    `json:"city"`
+	Street      string    `json:"street"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
---
+++ scripts/db/migrations/20250801120000_add_columns_to_stores.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE stores
+DROP COLUMN email,
+DROP COLUMN password,
+DROP COLUMN phone_number,
+DROP COLUMN zipcode;
